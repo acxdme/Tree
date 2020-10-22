b92352e80cd66434e89fd556bf52e0279e469de3
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-10-22 20:01:40 +0530

    Update RootToRootMaxSum.cpp

--- RootToRootMaxSum.cpp
+++ RootToRootMaxSum.cpp
@@ -3,3 +3,37 @@ Time complexity : O(N)
 Space complexity :
 My approach :
 */
+
+int helper(struct Node *root , int &res) 
+{ 
+    
+    if (root==NULL) return 0; 
+    if (!root->left && !root->right) return root->data; 
+  
+   
+    int ls = helper(root->left, res); 
+    int rs = helper(root->right, res); 
+   
+    if (root->left && root->right) 
+    { 
+        
+        res = max(res, ls + rs + root->data); 
+  
+        
+        return max(ls, rs) + root->data; 
+    } 
+  
+    return (root->left==false)? rs + root->data :   ls + root->data;
+    
+} 
+
+int maxPathSum(Node* root)
+{ 
+    if(root==NULL) return 0;
+     int result=INT_MIN;
+     helper(root,result);
+     
+     return result;
+    
+   
+}
